54f947d9afe63ed014f4a5e5edd6ee4257e4517f
SIdhesh Naik <siddeshnaik44@gmail.com>
2020-12-30 00:22:56 +0530

    renamed main activity to movie list activity

---
+++ SidheshNaikTenTwentyAssignment/app/src/main/java/com/example/sidheshnaiktentwentyassignment/ui/movielist/MovieListActivity.java
@@ -0,0 +1,16 @@
+package com.example.sidheshnaiktentwentyassignment.ui.movielist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sidheshnaiktentwentyassignment.R;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+    }
+}
